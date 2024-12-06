c3744d03e950f8731f9d940f5bbef4f8d0956a7c
Rudraksh Tyagi <rudrakshtyagi4281@gmail.com>
2024-12-06 08:59:39 +0530

    footer done

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function locomotiveAnimation(){
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("#main"),
+      smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+    
+
+
+}
+
+locomotiveAnimation()
+
+// locomotive animation ka function with calling
+
 function loadingAnimation() {     
     var tl = gsap.timeline()
 
@@ -83,6 +124,10 @@ function loadingAnimation() {
     })
 
 
+    tl.from("#hero1 , #page2",{
+opacity:0,
+    })
+
 
 }
 loadingAnimation()
@@ -110,5 +155,5 @@ y:dets.y
 
 }
 // shery js comment out kr rkhi hai ek bar use thik krna hai...
-cursorAnimation()
+// cursorAnimation()
 // cursor aur magnet wali animation with sheryjs
